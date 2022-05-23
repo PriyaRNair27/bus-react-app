cda5851e8bd9a0e9049b5fd74ae69a51cdcef8a6
PriyaRNair27 <priyarnair2710@gmail.com>
2022-05-23 19:55:45 +0530

    nav

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Addbus from './Components/Addbus';
 import Search from './Components/Search';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-<Addbus/>
-<Search/>
+<BrowserRouter>
+<Routes>
+  <Route path="/" exact element={<Addbus/>}/>
+  <Route path="/search" exact element={<Search/>}/>
+  <Route/>
+</Routes>
+</BrowserRouter>
     </div>
   );
 }
