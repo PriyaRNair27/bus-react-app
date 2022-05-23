4d7904e2437a63710ae9d2b83df27dcde05e4085
PriyaRNair27 <priyarnair2710@gmail.com>
2022-05-23 16:42:36 +0530

    new

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import Addbus from './Components/Addbus';
 
 function App() {
   return (
     <div>
-
+<Addbus/>
     </div>
   );
 }
